df9ba65db494c5d9aa90477fac965805957b8fe0
Sebastien Leboucher <sebastien.leboucher@gmail.com>
2020-03-02 20:12:36 +0100

    rename

--- src/main/java/com/example/project/dao/HelloRepository.java
+++ src/main/java/com/example/project/dao/HelloRepository.java
@@ -1,13 +1,5 @@
 /*
- * Copyright (c) 2001-2017 Group JCDecaux.
- * 17 rue Soyer, 92523 Neuilly Cedex, France.
- * All rights reserved.
- *
- * This software is the confidential and proprietary information
- * of Group JCDecaux ("Confidential Information").  You shall not
- * disclose such Confidential Information and shall use it only
- * in accordance with the terms of the license agreement you
- * entered into with Group JCDecaux.
+ * Sébastien Leboucher
  */
 package com.example.project.dao;
 
